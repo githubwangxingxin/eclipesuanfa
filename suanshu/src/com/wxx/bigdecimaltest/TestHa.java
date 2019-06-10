aad664ec3a53d2c8fb65a4bfdf3da2247938585b
githubwangxingxin <1615220543@qq.com>
2019-06-10 16:28:17 +0800

    提交v2

--- suanshu/src/com/wxx/bigdecimaltest/TestHa.java
+++ suanshu/src/com/wxx/bigdecimaltest/TestHa.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class TestHa {
 	public static void main(String[] args) {
 		Random random=new Random();
-		
+		System.out.println("aaaaaaaasa");
 		System.out.println("random.nextInt(1000)"+random.nextInt(10000));
 	}
 }
